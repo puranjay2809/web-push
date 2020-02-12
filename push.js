4f5867e3bd7a85b6c476fbc762ed980f2157f7b2
Puranjay <puranjay@clevertap.com>
2020-02-12 18:06:49 +0530

    fix for sw.js

--- push.js
+++ push.js
@@ -8,13 +8,13 @@ var push = require('web-push')
 // console.log(vapidkeys);
 
 let vapidkeys = {
-    publicKey: 'BH5WYjcHqWc99vvnbbH774IvweEMAZeDUQ4PUe_buWhtBsz3aCN55GdoY1Mc_mNrG8POBEYDXKAHQQi9PeXIO6U',
-    privateKey: 'mqXqz6WC6H2528LRHRILUS5WsCAwkYjoHeAyS-L_ZmE'
-  }
+  publicKey: 'BH5WYjcHqWc99vvnbbH774IvweEMAZeDUQ4PUe_buWhtBsz3aCN55GdoY1Mc_mNrG8POBEYDXKAHQQi9PeXIO6U',
+  privateKey: 'mqXqz6WC6H2528LRHRILUS5WsCAwkYjoHeAyS-L_ZmE'
+}
 
-  push.setVapidDetails('puranjay@clevertap.com',vapidkeys.publicKey, vapidkeys.privateKey)
+push.setVapidDetails('mailto:puranjay@clevertap.com', vapidkeys.publicKey, vapidkeys.privateKey)
 
-  //basicaslly the the client's subscribe data that we usually get from database
-  let sub = {};
+//basicaslly the the client's subscribe data that we usually get from database
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/eZbBgwI_a1o:APA91bFWyHowwQXNieZ-BliJGLxf4kvdHZeb0_aRa27VTIKbhuSPcscz3XKmkWBA7tZQ5WAVLI6NF08krqNxefpdJYoiPgjtg2GtC1NPfcsTeaeuxqI6MWjpOxFXVXu6JwA46v8JgLHt","expirationTime":null,"keys":{"p256dh":"BEMGROgM513MHW-hfLUEBmgY3MV97TAn1fL7PSnJmixp01SfEW4b-i5mj8CD4L9xU1YzDl-Bv8am7g1qzGYUQaw","auth":"mOjwOTqkqlLfQEmjDmNhDQ"}}
 
-  push.sendNotification(sub, 'Test Message')
+push.sendNotification(sub, 'Test Message')
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //This lives at the root folder of the client side
 self.addEventListener('push', function(e) {
-    var optoins ={
+    var options ={
         body: 'This notification welcomes to new world!',
         icon: 'images/example.png',
         vibrate: [100, 50, 100],
